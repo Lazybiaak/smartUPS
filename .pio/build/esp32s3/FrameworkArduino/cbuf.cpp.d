1e696f0bd20f2184faf7bca025fb4af69bb85acf
lazybiaak <mansubiku@gmail.com>
2025-05-14 22:24:32 +0545

    added config files

---
+++ .pio/build/esp32s3/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32s3/FrameworkArduino/cbuf.cpp.o: \
+ /home/biaak/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/biaak/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
